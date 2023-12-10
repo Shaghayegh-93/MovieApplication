cc86f59476b84cfe41ba33b382dcc97fc8685b1c
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-12-10 19:12:55 +0000

    add new fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+        system: ["system-ui", "sans-serif"],
+        Avenir: ["Avenir", "sans-serif"],
+        Helvetica: ["Helvetica", "sans-serif"],
+        Arial: ["Arial", "sans-serif"],
+      },
       colors: {
         navy: "#032038",
       },
