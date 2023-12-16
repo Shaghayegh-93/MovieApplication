63f7430f69bc5bc78ee2160998a7b451d51bff33
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-12-16 23:29:40 +0000

    add watchList and favorites state and add two function for get watchList and favorites

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,24 @@ import MovieInfo from "./components/MovieInfo";
 function App() {
   const [isloading, setIsLoading] = useState(false);
   const [movieList, setMovieList] = useState([]);
+  console.log(movieList);
+  const [favorite, setFavorite] = useState([]);
+  const [watchList, setWatchList] = useState([]);
+  console.log("favorite", favorite);
+
   const [movie, setMovie] = useState(null);
   const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
   const BASE_URL = "https://api.themoviedb.org/3";
   let url = BASE_URL + "/movie/popular?api_key=" + API_KEY;
   const [navUrl, setNavUrl] = useState(url);
   const [id, setId] = useState(null);
+  console.log("id", id);
 
-  let single = BASE_URL + `/movie/${id}`;
+  // let single = BASE_URL + `/movie/${id}`;
+  const addToFavorite = () => {
+    const favoriteMovie = movieList.find((movie) => movie.id).includes(id);
+    setFavorite(favoriteMovie);
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -71,49 +81,22 @@ function App() {
 
     setNavUrl(url);
   };
+  const addFavoriteHandler = (movieId) => {
+    const isAlreadyFavorite = favorite.some((fav) => fav.id === Number(movieId));
+    if (isAlreadyFavorite) return;
+    const favoriteMovie = movieList.find((fav) => fav.id === Number(movieId));
+    setFavorite((prev) => setFavorite([...prev, favoriteMovie]));
+  };
+   const addWatchList = (movieId) => {
+     const isAlreadyWatchList = favorite.some(
+       (fav) => fav.id === Number(movieId)
+     );
+     if (isAlreadyWatchList) return;
+     const watchMovie = movieList.find((movie) => movie.id === Number(movieId));
+     setWatchList((prev) => setWatchList([...prev, watchMovie]));
+   };
+  // const isAddToFavorites = favorite?.map((fav) => fav.id).includes(id);
 
-  //   case "popular":
-  //     {
-  //       const URL = BASE_URL + "/movie/popular?api_key=" + API_KEY;
-  //       setNavUrl(URL);
-  //     }
-  //     break;
-  //   case "Now Playing":
-  //     {
-  //       const URL =
-  //         BASE_URL +
-  //         "/movie/now_playing?language=en-US&page=1&api_key=" +
-  //         API_KEY;
-  //       setNavUrl(URL);
-  //     }
-  //     break;
-  //   case "Top Rated":
-  //     {
-  //       const URL =
-  //         BASE_URL +
-  //         "/movie/top_rated?language=en-US&page=1&api_key=" +
-  //         API_KEY;
-  //       setNavUrl(URL);
-  //     }
-  //     break;
-  //   case "Upcoming":
-  //     {
-  //       const URL =
-  //         BASE_URL + "/movie/upcoming?language=en-US&page=1&api_key=" + API_KEY;
-  //       setNavUrl(URL);
-  //     }
-  //     break;
-
-  //   default: {
-  //   }
-
-  // }
-
-  // const getSelectedMovieId=(id)=>{
-  //   setId(Number(id))
-  // }
-  // const routeParams = useParams();
-  // console.log("idddd", routeParams);
   return (
     <div className="">
       <Nav getMovieData={getMovieData} />
@@ -123,7 +106,18 @@ function App() {
           index
           element={<Movielist movieList={movieList} id={id} setId={setId} />}
         />
-        <Route path="/movie/:id" element={<MovieInfo movie={movie} />} />
+        <Route
+          path="/movie/:id"
+          element={
+            <MovieInfo
+              movie={movie}
+              addFavoriteHandler={addFavoriteHandler}
+              addWatchList={addWatchList}
+              // isAddToFavorites={isAddToFavorites}
+              // isAlreadyFavorite={isAlreadyFavorite}
+            />
+          }
+        />
       </Routes>
     </div>
   );
