2f115ce2431c2fc129543967bcf16d1685d61a37
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-12-18 23:34:30 +0000

    change the style of modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,7 +9,7 @@ const Modal = ({ isOpen, setIsOpen, children }) => {
         className="w-screen h-screen fixed inset-0 bg-slate-900  bg-opacity-70 "
         onClick={() => setIsOpen(false)}
       ></div>
-      <div className="h-full w-44 md:w-[500px]  min-h-[250px] absolute top-0 left-52 md:-left-96 -translate-x-full -translate-y-44 md:translate-y-1/2 bg-slate-800 p-4 rounded-2xl shadow-modalShadow">
+      <div className="h-auto w-44 md:w-[500px]  min-h-[250px] absolute top-0 left-52 md:-left-96 -translate-x-full -translate-y-44 md:translate-y-1/2 bg-slate-800 p-4 rounded-2xl shadow-modalShadow">
         <div className="flex items-center  justify-between mb-4 pb-2 border-b-[1px] border-slate-600">
           <h2 className="text-slate-200">title</h2>
           <button onClick={() => setIsOpen(false)}>
