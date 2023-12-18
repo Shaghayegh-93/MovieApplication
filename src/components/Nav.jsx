fdcfd2875f5c57481fb80588bdfe314be6a6118f
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-12-18 23:33:17 +0000

    add useLocation for conditional rendering input based on path

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ import {
   ListBulletIcon,
 } from "@heroicons/react/24/outline";
 
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import Modal from "./Modal";
 
 const Nav = ({
@@ -16,27 +16,31 @@ const Nav = ({
   favorite,
   watchList,
   removeFavoriteHandler,
-  removeWatchListHandler
+  removeWatchListHandler,
 }) => {
   const navItem = ["Popular", "Now Playing", "Top Rated", "Upcoming"];
   const [isNavOpen, setIsNavOpen] = useState(false);
- 
+  const location = useLocation();
+  const isMovielistRoute = location.pathname === "/";
+
   return (
     <nav className="bg-navy   py-4 md:px-24 px-8 shadow-lg ">
       <div className="flex items-center justify-between">
         <div className="text-white ">
-          <Link to="https://www.themoviedb.org/?language=en-GB">
+          <Link to="/">
             <h1 className="font-bold text-lg">Logo</h1>
           </Link>
         </div>
         <div>
-          <input
-            className="py-3 px-4 rounded-lg text-slate100 bg-slate500 text-base hidden md:inline-block"
-            type="text"
-            value={search}
-            placeholder="Search..."
-            onChange={(e) => setSearch(e.target.value)}
-          />
+        
+          {isMovielistRoute && (
+            <input
+              type="text"
+              placeholder="Search movies..."
+              onChange={(e) => setSearch(e.target.value)}
+              className="py-3 px-4 rounded-lg text-slate100 bg-slate500 text-base hidden md:inline-block"
+            />
+          )}
         </div>
 
         <ul
@@ -104,10 +108,7 @@ function Favorites({ favorite, removeFavoriteHandler }) {
           </div>
         ))}
       </Modal>
-      <button
-        onClick={() => setIsOpen((is) => !is)}
-        className="relative  "
-      >
+      <button onClick={() => setIsOpen((is) => !is)} className="relative  ">
         <HeartIcon className="w-8 h-8 text-red-600" />
         <span className="absolute text-xs top-0 -right-2 h-4 leading-4 text-center bg-red-600 text-white rounded-full   py-2 px-[5px]  flex items-center justify-center">
           {favorite?.length}
@@ -129,7 +130,7 @@ function WatchList({ watchList, removeWatchListHandler }) {
             key={movie.id}
           >
             <div>
-              <p>{movie?.title}</p>
+              <p>{movie.title}</p>
             </div>
             <button onClick={() => removeWatchListHandler(movie.id)}>
               <TrashIcon className="w-6 h-6 text-rose-500" />
@@ -137,13 +138,10 @@ function WatchList({ watchList, removeWatchListHandler }) {
           </div>
         ))}
       </Modal>
-      <button
-        onClick={() => setIsOpen((is) => !is)}
-        className="relative "
-      >
+      <button onClick={() => setIsOpen((is) => !is)} className="relative ">
         <ListBulletIcon className="w-8 h-8 text-red-600" />
         <span className="absolute text-xs top-0 -right-2 h-4 leading-4 text-center bg-red-600 text-white rounded-full   py-2 px-[5px]  flex items-center justify-center">
-          {watchList?.length}
+          {watchList.length}
         </span>
       </button>
     </>
