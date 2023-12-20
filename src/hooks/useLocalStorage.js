d766c0cd323c5b8c7dc26e0181010f07f339237c
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-12-20 20:37:43 +0000

    change the whole logic

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,22 +1,48 @@
+// import { useEffect, useState } from "react";
+
+// export default function useLocalStorage({ key, initialState }) {
+//   // const [value, setValue] = useState(
+//   //   () => JSON.parse(localStorage.getItem(key)) || initialState
+//   // );
+//    const [value, setValue] = useState(() => {
+//      try {
+//        const storedValue = localStorage.getItem(key);
+//        return storedValue ? JSON.parse(storedValue) : initialState;
+//      } catch (error) {
+//        console.error(`Error parsing local storage for key "${key}":`, error);
+//        return initialState;
+//      }
+//    });
+
+//   useEffect(() => {
+//     localStorage.setItem(key, JSON.stringify(value));
+//   }, [value]);
+
+//   return [value, setValue];
+// }
 import { useEffect, useState } from "react";
 
 export default function useLocalStorage({ key, initialState }) {
-  // const [value, setValue] = useState(
-  //   () => JSON.parse(localStorage.getItem(key)) || initialState
-  // );
-   const [value, setValue] = useState(() => {
-     try {
-       const storedValue = localStorage.getItem(key);
-       return storedValue ? JSON.parse(storedValue) : initialState;
-     } catch (error) {
-       console.error(`Error parsing local storage for key "${key}":`, error);
-       return initialState;
-     }
-   });
+  const [value, setValue] = useState(() => {
+    try {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialState;
+    } catch (error) {
+      console.error(`Error parsing local storage for key "${key}":`, error);
+      return initialState;
+    }
+  });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [value]);
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(
+        `Error storing data in local storage for key "${key}":`,
+        error
+      );
+    }
+  }, [key, value]);
 
   return [value, setValue];
 }
