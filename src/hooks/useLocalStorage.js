01a7f791ed8c627d1dabf0c27fa329d94c2f4635
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-12-20 20:31:00 +0000

    try to find a bug

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 
 export default function useLocalStorage({ key, initialState }) {
-  const [value, setValue] = useState(
-    () => JSON.parse(localStorage.getItem(key)) || initialState
-  );
+  // const [value, setValue] = useState(
+  //   () => JSON.parse(localStorage.getItem(key)) || initialState
+  // );
+   const [value, setValue] = useState(() => {
+     try {
+       const storedValue = localStorage.getItem(key);
+       return storedValue ? JSON.parse(storedValue) : initialState;
+     } catch (error) {
+       console.error(`Error parsing local storage for key "${key}":`, error);
+       return initialState;
+     }
+   });
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
